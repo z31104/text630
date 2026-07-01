ed4bae6e80454c4f8817660c09f04ce866c71fb3
chingchen1126 <huangroger1126@gmail.com>
2026-07-01 14:28:53 +0800

    web:完成首頁前端模型

--- routes/home.py
+++ routes/home.py
@@ -1,10 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/")
 def home():
-    return """
-    <h1>智慧會員辨識系統</h1>
-    <p>系統啟動成功</p>
-    """
+    return render_template("index.html")
