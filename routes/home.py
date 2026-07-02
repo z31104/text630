29042e8680ad27ad88f8d2ac9d48de933ff8ac19
chingchen1126 <huangroger1126@gmail.com>
2026-07-02 15:35:04 +0800

    web:Add dashboard page

--- routes/home.py
+++ routes/home.py
@@ -5,3 +5,9 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def home():
     return render_template("index.html")
+
+@home_bp.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
